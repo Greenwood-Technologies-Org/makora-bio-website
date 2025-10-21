5fc664a15b1c5ab60199d1699adb4e78bca8d9d5
Roshan Kern <roshan.kern@ucdenver.edu>
2025-10-21 09:56:48 -0700

    fix tailwind styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,8 @@ import type { Config } from "tailwindcss";
 
 export default {
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
